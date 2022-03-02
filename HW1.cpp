8a892ba67ea9b9b16d40fefe0f1dc260ed4bfccc
Hentci <1222bruce@gmail.com>
2022-03-02 14:46:13 +0800

    ac

--- HW1.cpp
+++ HW1.cpp
@@ -2,24 +2,31 @@
 #include <vector>
 #include <map>
 #include <array>
+#include <queue>
+#include <functional>
 using namespace std;
 using ui = unsigned int;
+#define ll long long
 #define ar array
 const int mxn = 2e3;
 class node{
     public:
-
     ui id;
-
 };
-int G[mxn][mxn];
+class edge{
+    public:
+    bool isold;
+    ui neighbor;
+    ll w;
+    edge(){}
+    edge(bool a, int b, ll c): isold(a), neighbor(b), w(c){}
+};
 int main(){
     // freopen("input.txt", "r", stdin);
     int n, dst, m; // n -> the number of nodes, m -> the number of links
     cin >> n >> dst >> m;
+    vector <vector <edge>> G(mxn);
     vector <node> dsts(dst); 
-    // vector <vector <ar<ui, 3>>> adj(n); // to store the neighbor's IDs <b, oldw, neww>
-    // vector <map<node, node>> table(n); // to store each entry <destination ID, next node ID>
     map<ui, map<ui, ui>> table;
     
 
@@ -28,94 +35,74 @@ int main(){
         dsts[i].id = nd;
     }
 
-    // ui a, b, ow, nw;
-    // for(int i = 0, linkid;i < m;i++){
-    //     cin >> linkid >> a >> b >> ow >> nw;
-    //     adj[a].push_back({b, ow, nw});
-    //     adj[b].push_back({a, ow, nw});
-    // }
-
-    map <int, ar<int, 3>> links;
+    // map <int, ar<int, 3>> links; // <a, linkid, b, w> linkid -> useless
     int a, b, linkid;
-    for(int i = 0, ow, nw;i < m;i++){
+    ll ow, nw;
+    for(int i = 0;i < m;i++){
         cin >> linkid >> a >> b >> ow >> nw;
-        G[a][b] = ow;
-        G[b][a] = ow;
-        links[linkid] = {a, b, nw};
+        G[a].push_back(edge(true, b, ow));
+        G[b].push_back(edge(true, a, ow));
+        G[a].push_back(edge(false, b, nw));
+        G[b].push_back(edge(false, a, nw));
     }
 
-    // for(int i = 0;i < n;i++){
-    //     for(int j = 0;j < n;j++)
-    //         cout << G[i][j] << " ";
-    //     cout << "\n";
-    // }
-
-    vector <int> d(n);
-    vector <bool> inPath(n);
+    vector <ll> d(n);
+    // vector <bool> inPath(n);
     vector <ui> par(n);
-    auto dijk = [&](ui st) -> void{
-        fill(d.begin(), d.end(), INT32_MAX);
-        fill(inPath.begin(), inPath.end(), false);
-        par[0] = -1;
+    auto dijk = [&](bool ok, ui st) -> void{
+        fill(d.begin(), d.end(), 0x3f3f3f3f3f);
         d[st] = 0;
-        for(int i = 0;i < n - 1;i++){
-            int mn = INT32_MAX, mnIdx;
-            for(int j = 0;j < n;j++)
-                if(!inPath[j] && d[j] <= mn)
-                    mn = d[j], mnIdx = j;
-
-            inPath[mnIdx] = true;
-            for(int v = 0;v < n;v++){
-                if(!inPath[v] && G[mnIdx][v] && d[mnIdx] + G[mnIdx][v] <= d[v]){
-                    par[v] = mnIdx;
-                    d[v] = d[mnIdx] + G[mnIdx][v];
+        par[st] = st;
+        priority_queue <ar<ll, 2>, vector <ar<ll, 2>>, greater<ar<ll, 2>>> pq;
+        pq.push({0, st});
+        while(!pq.empty()){
+            auto [wei, v] = pq.top(); pq.pop();
+            if(wei > d[v]) continue;
+            for(auto ele: G[v]){
+                if(ele.isold == ok) continue;
+                // upd smaller node if they are same d
+                if(d[ele.neighbor] == d[v] + ele.w && v < par[ele.neighbor])
+                    par[ele.neighbor] = v;
+                if(d[ele.neighbor] > d[v] + ele.w){
+                    par[ele.neighbor] = v;
+                    d[ele.neighbor] = d[v] + ele.w;
+                    pq.push({d[ele.neighbor], ele.neighbor});
                 }
             }
         }
     };
 
-    // dijk(4);
-    // for(auto ele: par)
-    //     cout << ele << " ";
-    // cout << "\n";
     for(auto ele: dsts){
-        dijk(ele.id);
-        for(ui v = 0;v < n;v++){
-            if(v == ele.id) continue;
-            table[v][ele.id] = par[v];
+        dijk(0, ele.id);
+        for(int i = 0;i < n;i++){
+            if(i == (int)ele.id) continue;
+            table[i][ele.id] = par[i];
         }
     }
     for(int i = 0;i < n;i++){
-        if(table[i].empty()) continue;
         cout << i << "\n";
-        for(auto ele: table[i]){
+        for(auto ele: table[i])
             cout << ele.first << " " << ele.second << "\n";
-        }
     }
 
-    for(auto ele: links){
-        G[ele.second[0]][ele.second[1]] = ele.second[2];
-        G[ele.second[1]][ele.second[0]] = ele.second[2];
-    }
-    // bool skip[n][n] = {};
     for(auto ele: dsts){
-        dijk(ele.id);
-        for(ui v = 0;v < n;v++){
-            if(v == ele.id) continue;
-            if(par[v] == table[v][ele.id]){
-                table[v].erase(ele.id);
+        dijk(1, ele.id);
+        for(int i = 0;i < n;i++){
+            if(i == (int)ele.id) continue;
+            if(par[i] == table[i][ele.id]){
+                table[i].erase(ele.id);
                 continue;
             }
-            table[v][ele.id] = par[v];
+            table[i][ele.id] = par[i];
         }
     }
     for(int i = 0;i < n;i++){
         if(table[i].empty()) continue;
         cout << i << "\n";
-        for(auto ele: table[i]){
+        for(auto ele: table[i])
             cout << ele.first << " " << ele.second << "\n";
-        }
     }
+
 
     return 0;
 }
